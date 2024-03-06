1eded5b0bac386e753e2d9c2e9b9671d43595f6f
Shivambhayani <156076493+Shivambhayani@users.noreply.github.com>
2024-03-06 15:13:05 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const sequelize = Sequelize;
 
 
 
-const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new sequelize(to_do, root, password$1, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
