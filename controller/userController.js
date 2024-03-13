e87436d5c9c4c803a211bc5b48cf9293b549cac1
Shivam <shivam01.tst@gmail.com>
2024-03-13 09:47:31 +0530

    name aaded in login time

--- controller/userController.js
+++ controller/userController.js
@@ -46,7 +46,7 @@ const signUp = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const { name, email, password } = req.body;
         if (!email || !password) {
             return res.status(400).json({
                 status: "fail",
